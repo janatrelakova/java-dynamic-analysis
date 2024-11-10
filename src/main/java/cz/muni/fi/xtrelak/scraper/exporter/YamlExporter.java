944826329e427efd686a4b05f344cf02c9fb20a7
Jana Treláková <jana.trelakova@gmail.com>
2024-11-10 23:47:25 +0100

    Cleanup YAML Exporter

--- src/main/java/cz/muni/fi/xtrelak/scraper/exporter/YamlExporter.java
+++ src/main/java/cz/muni/fi/xtrelak/scraper/exporter/YamlExporter.java
@@ -3,23 +3,18 @@ package cz.muni.fi.xtrelak.scraper.exporter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import cz.muni.fi.xtrelak.scraper.HTTP_METHOD;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 public class YamlExporter {
     public void export(List<EndpointOutput> endpoints) {
-        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
-
         endpoints.sort((a, b) -> {
             if (a.url.equals(b.url)) {
                 return a.httpMethod.compareTo(b.httpMethod);
             }
             return a.url.compareTo(b.url);
         });
-        var x = new EndpointOutput(HTTP_METHOD.GET, "/products", null, null, null);
         printAsYAML(endpoints);
 
     }
