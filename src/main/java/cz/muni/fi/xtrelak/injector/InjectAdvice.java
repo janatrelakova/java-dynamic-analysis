1fbb944007d605a99010928b5e253a595a43f08e
Jana Treláková <jana.trelakova@gmail.com>
2024-11-27 15:07:42 +0100

    Injector - print to the file and add logging

--- src/main/java/cz/muni/fi/xtrelak/injector/InjectAdvice.java
+++ src/main/java/cz/muni/fi/xtrelak/injector/InjectAdvice.java
@@ -1,11 +1,12 @@
 package cz.muni.fi.xtrelak.injector;
 
 import net.bytebuddy.asm.Advice;
-
-import java.util.Arrays;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class InjectAdvice {
+    private static final Logger logger = LogManager.getLogger(InjectAdvice.class);
+
     @Advice.OnMethodEnter
     public static void onEnter(@Advice.Origin("#m") String methodName, @Advice.Origin("#t") String className) {
         StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
@@ -13,9 +14,9 @@ public class InjectAdvice {
             // Caller should be on the third position, but we have altered the stack
             // by calling `Thread.currentThread().getStackTrace();`
             StackTraceElement caller = stackTrace[2];
-            System.out.println(caller.getClassName() + "." + caller.getMethodName() + " -> " + className + "." + methodName);
+            Writer.writeToFile(caller.getClassName() + "." + caller.getMethodName() + " -> " + className + "." + methodName);
         } else {
-            System.out.println("Method: " + methodName + " has no caller information available.");
+            logger.info("Method: {} has no caller information available.", methodName);
         }
     }
 }
